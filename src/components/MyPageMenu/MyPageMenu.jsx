e9358784c3102c251db316e441666e934d4389b8
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-11-26 23:27:29 +0900

    fix: 이미지 src 경로 변경경

--- src/components/MyPageMenu/MyPageMenu.jsx
+++ src/components/MyPageMenu/MyPageMenu.jsx
@@ -1,21 +1,18 @@
 import './MyPageMenu.css';
-import SettingIcon from '/src/assets/setting.svg';
-import LogoutIcon from '/src/assets/logout.svg';
-import UserDeleteIcon from '/src/assets/user-delete.svg';
 
 const MyPageMenu = () => {
   return (
     <div className="sub_mypage_info_item">
       <div>
-        <img src={SettingIcon} alt="profile" className="item_icon" />
+        <img src='./setting.svg' alt="profile" className="item_icon" />
         <p>프로필 설정</p>
       </div>
       <div>
-        <img src={LogoutIcon} alt="profile" className="item_icon" />
+        <img src='./logout.svg' alt="profile" className="item_icon" />
         <p>로그아웃</p>
       </div>
       <div>
-        <img src={UserDeleteIcon} alt="profile" className="item_icon" />
+        <img src='./user-delete.svg' alt="profile" className="item_icon" />
         <p>탈퇴하기</p>
       </div>
     </div>
