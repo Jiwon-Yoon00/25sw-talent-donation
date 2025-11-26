28910d31205619b94588143e98b812df926bb789
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-11-27 00:07:06 +0900

    [chore] MyPage 디렉토리로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header'
-import MyPage from './pages/MyPage.jsx'
+import MyPage from './pages/MyPage/MyPage.jsx'
+// import LongTypingPractice from './pages/LongTypingPractice/LongTypingPractice.jsx'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />*/}
         <Route path="/mypage" element={<MyPage />} /> 
+        {/* <Route path="/long-typing-practice" element={<LongTypingPractice />} /> */}
       </Routes>
     </Router>
   );
