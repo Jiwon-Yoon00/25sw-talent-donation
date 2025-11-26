31e8667f743ae25f98d5f2b02221abba37a571fe
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-11-26 23:27:29 +0900

    fix: 이미지 src 경로 변경경

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,10 +1,11 @@
 import './ProfileCard.css';
-import DogImage from '/src/assets/dog.jpg';
+
+
 
 const ProfileCard = () => {
   return (
     <div>
-      <img src={DogImage} alt="profile" className="profile_image" />
+      <img src='/dog.jpg' alt="profile" className="profile_image" />
       <p className="profile_name">뭉이</p>
       <p>한국항공대학교</p>
     </div>
