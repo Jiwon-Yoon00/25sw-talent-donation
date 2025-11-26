5a2a2788fd559d2ac3f34eb49cc13dd1804725eb
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-11-26 19:29:20 +0900

    fix: 이미지 파일 src -> public으로 이동동

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,9 +1,10 @@
 import './ProfileCard.css';
+import DogImage from '/src/assets/dog.jpg';
 
 const ProfileCard = () => {
   return (
     <div>
-      <img src="/src/assets/dog.jpg" alt="profile" className="profile_image" />
+      <img src={DogImage} alt="profile" className="profile_image" />
       <p className="profile_name">뭉이</p>
       <p>한국항공대학교</p>
     </div>
