7e054d79481db694dfd21ab231849536bdb7c3fe
Jiwon-Yoon00 <0yuniverse0@gmail.com>
2025-12-16 00:57:04 +0900

    feat: 마이페이지 api 연결

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,12 +1,10 @@
-import './ProfileCard.css';
+import "./ProfileCard.css";
 
-
-
-const ProfileCard = () => {
+const ProfileCard = ({ userId }) => {
   return (
     <div>
-      <img src='/dog.jpg' alt="profile" className="profile_image" />
-      <p className="profile_name">뭉이</p>
+      <img src="/dog.jpg" alt="profile" className="profile_image" />
+      <p className="profile_name">{userId || "뭉이"}</p>
       <p>한국항공대학교</p>
     </div>
   );
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,14 +1,14 @@
 import ProfileCard from "../ProfileCard/ProfileCard";
-import './SideBar.css';
+import "./SideBar.css";
 
-
-const SideBar = ({title, menu}) => {
-  return(
+const SideBar = ({ title, menu, userId }) => {
+  return (
     <aside className="side_bar">
       <h2>{title}</h2>
-      <ProfileCard />
+      <ProfileCard userId={userId} />
       {menu}
     </aside>
-  )}
+  );
+};
 
 export default SideBar;
